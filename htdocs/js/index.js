cf8719f9d95a22d54fafaee712a7d3fdf80d5ae3
tkompare <tom@kompare.us>
2018-07-24 21:28:51 -0500

    Map style and event modal formatting
    Map style and event modal formatting

--- htdocs/js/index.js
+++ htdocs/js/index.js
@@ -4,203 +4,41 @@ function initMap() {
 			lat: 41.875,
 			lng: -87.6425
 		},
+		// Map Style from: https://snazzymaps.com/style/24358/blue
 		styles: [
 			{
-				"featureType": "poi.business",
+				"featureType": "all",
+				"elementType": "all",
+				"stylers": [
+					{
+						"hue": "#075290"
+					}
+				]
+			},
+			{
+				"featureType": "poi",
+				"elementType": "all",
 				"stylers": [
 					{
 						"visibility": "off"
-					}
-				]
-			},
-			{
-				"elementType": "geometry",
-				"stylers": [
-					{
-						"color": "#ebe3cd"
-					}
-				]
-			},
-			{
-				"elementType": "labels.text.fill",
-				"stylers": [
-					{
-						"color": "#523735"
-					}
-				]
-			},
-			{
-				"elementType": "labels.text.stroke",
-				"stylers": [
-					{
-						"color": "#f5f1e6"
-					}
-				]
-			},
-			{
-				"featureType": "administrative",
-				"elementType": "geometry.stroke",
-				"stylers": [
-					{
-						"color": "#c9b2a6"
-					}
-				]
-			},
-			{
-				"featureType": "administrative.land_parcel",
-				"elementType": "geometry.stroke",
-				"stylers": [
-					{
-						"color": "#dcd2be"
-					}
-				]
-			},
-			{
-				"featureType": "administrative.land_parcel",
-				"elementType": "labels.text.fill",
-				"stylers": [
-					{
-						"color": "#ae9e90"
-					}
-				]
-			},
-			{
-				"featureType": "landscape.natural",
-				"elementType": "geometry",
-				"stylers": [
-					{
-						"color": "#dfd2ae"
-					}
-				]
-			},
-			{
-				"featureType": "poi",
-				"elementType": "geometry",
-				"stylers": [
-					{
-						"color": "#dfd2ae"
-					}
-				]
-			},
-			{
-				"featureType": "poi",
-				"elementType": "labels.text.fill",
-				"stylers": [
-					{
-						"color": "#93817c"
-					}
-				]
-			},
-			{
-				"featureType": "poi.park",
-				"elementType": "geometry.fill",
-				"stylers": [
-					{
-						"color": "#a5b076"
-					}
-				]
-			},
-			{
-				"featureType": "poi.park",
-				"elementType": "labels.text.fill",
-				"stylers": [
-					{
-						"color": "#447530"
 					}
 				]
 			},
 			{
 				"featureType": "road",
-				"elementType": "geometry",
+				"elementType": "all",
 				"stylers": [
 					{
-						"color": "#f5f1e6"
+						"saturation": "0"
+					},
+					{
+						"lightness": "0"
 					}
 				]
 			},
 			{
-				"featureType": "road.arterial",
-				"elementType": "geometry",
-				"stylers": [
-					{
-						"color": "#fdfcf8"
-					}
-				]
-			},
-			{
-				"featureType": "road.highway",
-				"elementType": "geometry",
-				"stylers": [
-					{
-						"color": "#f8c967"
-					}
-				]
-			},
-			{
-				"featureType": "road.highway",
-				"elementType": "geometry.stroke",
-				"stylers": [
-					{
-						"color": "#e9bc62"
-					}
-				]
-			},
-			{
-				"featureType": "road.highway.controlled_access",
-				"elementType": "geometry",
-				"stylers": [
-					{
-						"color": "#e98d58"
-					}
-				]
-			},
-			{
-				"featureType": "road.highway.controlled_access",
-				"elementType": "geometry.stroke",
-				"stylers": [
-					{
-						"color": "#db8555"
-					}
-				]
-			},
-			{
-				"featureType": "road.local",
-				"elementType": "labels.text.fill",
-				"stylers": [
-					{
-						"color": "#806b63"
-					}
-				]
-			},
-			{
-				"featureType": "transit.line",
-				"elementType": "geometry",
-				"stylers": [
-					{
-						"color": "#dfd2ae"
-					}
-				]
-			},
-			{
-				"featureType": "transit.line",
-				"elementType": "labels.text.fill",
-				"stylers": [
-					{
-						"color": "#8f7d77"
-					}
-				]
-			},
-			{
-				"featureType": "transit.line",
-				"elementType": "labels.text.stroke",
-				"stylers": [
-					{
-						"color": "#ebe3cd"
-					}
-				]
-			},
-			{
-				"featureType": "transit.station",
+				"featureType": "transit",
+				"elementType": "all",
 				"stylers": [
 					{
 						"visibility": "off"
@@ -209,45 +47,39 @@ function initMap() {
 			},
 			{
 				"featureType": "water",
-				"elementType": "geometry.fill",
+				"elementType": "all",
 				"stylers": [
 					{
-						"color": "#b9d3c2"
-					}
-				]
-			},
-			{
-				"featureType": "water",
-				"elementType": "labels.text.fill",
-				"stylers": [
+						"visibility": "simplified"
+					},
 					{
-						"color": "#92998d"
+						"saturation": "-60"
+					},
+					{
+						"lightness": "-20"
 					}
 				]
 			}
 		]
-	};
-
-	let
-		Map = new google.maps.Map(document.getElementById('map'), {
-			zoom: 12,
-			center: Default.center,
-			styles: Default.styles,
-			clickableIcons: false,
-			mapTypeControl: false,
-			panControl: false,
-			streetViewControl: false,
-			zoomControl: true,
-			maxZoom: 18,
-			minZoom: 10,
-			zoomControlOptions: {
-				position: google.maps.ControlPosition.RIGHT_TOP
-			},
-			fullscreenControl: false
-		}),
-		//infoWindow = new google.maps.InfoWindow(),
-		marker,
-		i;
+	},
+	Map = new google.maps.Map(document.getElementById('map'), {
+		zoom: 12,
+		center: Default.center,
+		styles: Default.styles,
+		clickableIcons: false,
+		mapTypeControl: false,
+		panControl: false,
+		streetViewControl: false,
+		zoomControl: true,
+		maxZoom: 18,
+		minZoom: 10,
+		zoomControlOptions: {
+			position: google.maps.ControlPosition.RIGHT_TOP
+		},
+		fullscreenControl: false
+	}),
+	marker,
+	i;
 
 	$.getJSON('https://sheets.googleapis.com/v4/spreadsheets/1_HTPvKSlLnWP__Lq_r-mCYKGcLau4Z7MmlsSyCMc454/values/Sheet1!A1:V?majorDimension=ROWS&key=AIzaSyAixqsNXzEBfYRAvx1aPVeNqDSR5bIfBeU', function(EventData) {
 
@@ -260,22 +92,31 @@ function initMap() {
 			google.maps.event.addListener(marker, 'click', (function(marker, i) {
 				return function() {
 					$('#modal-event-detail-title').html(EventData['values'][i][6]);
+					let body = '<p>'+EventData['values'][i][0];
+					if(EventData['values'][i][1].trim() !== ''){
+						body += ' '+EventData['values'][i][1];
+					}
+					body += '<br>'+EventData['values'][i][2]+', '+EventData['values'][i][3]+' '+EventData['values'][i][4];
+					let beginDate = mdyToDate(EventData['values'][i][10]),
+						formattedBeginDate = intToDayName(beginDate.getUTCDay())+', '+intToMonthName(beginDate.getUTCMonth())+' '+beginDate.getUTCDate()+', '+beginDate.getUTCFullYear();
+					body += '<hr>'+formattedBeginDate;
 					// Is this a single day event?
 					if(EventData['values'][i][10] === EventData['values'][i][11]) {
-						let beginDate = mdyToDate(EventData['values'][i][10]);
-						$('#modal-event-detail-body').html(
-							"<p>"
-							+intToDayName(beginDate.getUTCDay())+', '+intToMonthName(beginDate.getUTCMonth())+' '+beginDate.getUTCDate()+', '+beginDate.getUTCFullYear()
-							+"</p>"
-						);
-
+						body += '<hr>Hours: '+EventData['values'][i][12]+' to '+EventData['values'][i][13];
+						// Make the ical! https://github.com/nwcell/ics.js
+						let cal = new ics();
+						cal.addEvent(EventData['values'][i][6], EventData['values'][i][18], EventData['values'][i][19], EventData['values'][i][10]+' '+EventData['values'][i][12], EventData['values'][i][11]+' '+EventData['values'][i][13]);
+						$('#modal-event-detail-ical').on('click', function(){
+							cal.download();
+						});
+					} else {
+						let endDate = mdyToDate(EventData['values'][i][11]),
+							formattedEndDate = intToDayName(endDate.getUTCDay())+', '+intToMonthName(endDate.getUTCMonth())+' '+endDate.getUTCDate()+', '+endDate.getUTCFullYear();
+						body += ' to '+formattedEndDate;
 					}
+					body += '<p>';
+					$('#modal-event-detail-body').html(body);
 					$('#modal-event-detail').modal('show');
-
-					// Make the ical! https://github.com/nwcell/ics.js
-					let cal = new ics();
-					cal.addEvent(EventData['values'][i][6], EventData['values'][i][18], EventData['values'][i][19], EventData['values'][i][10], EventData['values'][i][11]);
-					cal.download();
 				}
 			})(marker, i));
 		}
