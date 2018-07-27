2b25c5f20497312b6f0a4e6fb1ef46d09f36baef
tkompare <tom@kompare.us>
2018-07-26 21:49:39 -0500

    map/reduce
    Use map/reduce to transform Sheet data to an array of objects using the first 'row' to define properties

--- htdocs/js/index.js
+++ htdocs/js/index.js
@@ -80,7 +80,14 @@ var Vaccinate = {
 	initialize: function(){
 		$.when(
 			$.getJSON('https://sheets.googleapis.com/v4/spreadsheets/'+Vaccinate.Configs.GoogleSheets.sheetID+'/values/'+Vaccinate.Configs.GoogleSheets.values+'?majorDimension='+Vaccinate.Configs.GoogleSheets.majorDimension+'&key='+Vaccinate.Configs.GoogleSheets.key, function(events) {
-				Vaccinate.Events = events;
+				// Use map/reduce to transform Sheet data to an array of objects using the first 'row' to define properties
+				var keys = events.values.shift();
+				Vaccinate.Events = events.values.map(function(values) {
+					return keys.reduce(function(object, key, i) {
+						object[key] = values[i];
+						return object;
+					}, {});
+				});
 			})
 		).then(function(){
 			Vaccinate.Map = new google.maps.Map(document.getElementById('map'), {
@@ -99,32 +106,32 @@ var Vaccinate = {
 				},
 				fullscreenControl: false
 			});
-			for (Vaccinate.i = 0; Vaccinate.i < Vaccinate.Events['values'].length; Vaccinate.i++) {
+			for (Vaccinate.i = 0; Vaccinate.i < Vaccinate.Events.length; Vaccinate.i++) {
 				Vaccinate.Marker = new google.maps.Marker({
-					position: new google.maps.LatLng(Vaccinate.Events['values'][Vaccinate.i][20], Vaccinate.Events['values'][Vaccinate.i][21]),
+					position: new google.maps.LatLng(Vaccinate.Events[Vaccinate.i]['Latitude'], Vaccinate.Events[Vaccinate.i]['Longitude']),
 					map: Vaccinate.Map
 				});
 				google.maps.event.addListener(Vaccinate.Marker, 'click', (function(marker, i) {
 					return function() {
-						$('#modal-event-detail-title').html(Vaccinate.Events['values'][i][6]);
-						var body = '<p>'+Vaccinate.Events['values'][i][0];
-						if(Vaccinate.Events['values'][i][1].trim() !== ''){
-							body += ' '+Vaccinate.Events['values'][i][1];
+						$('#modal-event-detail-title').html(Vaccinate.Events[i]['LocationName']);
+						var body = '<p>'+Vaccinate.Events[i]['Address1'];
+						if(Vaccinate.Events[i]['Address2'].trim() !== ''){
+							body += ' '+Vaccinate.Events[i]['Address2'];
 						}
-						body += '<br>'+Vaccinate.Events['values'][i][2]+', '+Vaccinate.Events['values'][i][3]+' '+Vaccinate.Events['values'][i][4];
-						var beginDate = Vaccinate.mdyToDate(Vaccinate.Events['values'][i][10]),
+						body += '<br>'+Vaccinate.Events[i]['City']+', '+Vaccinate.Events[i]['State']+' '+Vaccinate.Events[i]['PostalCode'];
+						var beginDate = Vaccinate.mdyToDate(Vaccinate.Events[i]['BeginDate']),
 							formattedBeginDate = Vaccinate.intToDayName(beginDate.getUTCDay())+', '+Vaccinate.intToMonthName(beginDate.getUTCMonth())+' '+beginDate.getUTCDate()+', '+beginDate.getUTCFullYear();
 						body += '<hr>'+formattedBeginDate;
 						// Is this a single day event?
-						if(Vaccinate.Events['values'][i][10] === Vaccinate.Events['values'][i][11]) {
-							body += '<hr>Hours: '+Vaccinate.Events['values'][i][12]+' to '+Vaccinate.Events['values'][i][13];
+						if(Vaccinate.Events[i]['BeginDate'] === Vaccinate.Events[i]['EndDate']) {
+							body += '<hr>Hours: '+Vaccinate.Events[i]['BeginTime']+' to '+Vaccinate.Events[i]['EndTime'];
 							var cal = new ics(); // Make the ical! https://github.com/nwcell/ics.js
-							cal.addEvent(Vaccinate.Events['values'][i][6], Vaccinate.Events['values'][i][18], Vaccinate.Events['values'][i][19], Vaccinate.Events['values'][i][10]+' '+Vaccinate.Events['values'][i][12], Vaccinate.Events['values'][i][11]+' '+Vaccinate.Events['values'][i][13]);
+							cal.addEvent(Vaccinate.Events[i]['LocationName'], Vaccinate.Events[i]['NotesText'], Vaccinate.Events[i]['FormattedAddress'], Vaccinate.Events[i]['BeginDate']+' '+Vaccinate.Events[i]['BeginTime'], Vaccinate.Events[i]['EndDate']+' '+Vaccinate.Events[i]['EndTime']);
 							$('#modal-event-detail-ical').on('click', function(){
 								cal.download();
 							});
 						} else {
-							var endDate = Vaccinate.mdyToDate(Vaccinate.Events['values'][i][11]),
+							var endDate = Vaccinate.mdyToDate(Vaccinate.Events[i]['EndDate']),
 								formattedEndDate = Vaccinate.intToDayName(endDate.getUTCDay())+', '+Vaccinate.intToMonthName(endDate.getUTCMonth())+' '+endDate.getUTCDate()+', '+endDate.getUTCFullYear();
 							body += ' to '+formattedEndDate;
 						}
